b4a032e462a6ae1d7d41afecd251a1338a6309cf
eileen819 <eileen.ju.8819@gmail.com>
2024-04-08 20:18:31 +0900

    movie.js 파일 오타 수정

--- api/movie.js
+++ api/movie.js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
   const { title, page, id } = JSON.parse(request.body)
   const url = id 
     ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full` 
-    : `https://omdbapi.com?apikey=${APIKEY}=${title}&page=${page}`
+    : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`
   const res = await fetch(url)
   const json = await res.json()
   response
