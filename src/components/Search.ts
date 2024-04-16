1e2a495014b24612c6564d352b539b4a2db872d8
eileen819 <eileen.ju.8819@gmail.com>
2024-04-16 22:55:43 +0900

    compiled Typescript

--- src/components/Search.ts
+++ src/components/Search.ts
@@ -14,10 +14,10 @@ export default class Search extends Component {
     `
 
     const inputEl = this.el.querySelector('input')
-    inputEl.addEventListener('input', () => {
+    inputEl?.addEventListener('input', () => {
       movieStore.state.searchText = inputEl.value
     })
-    inputEl.addEventListener('keydown', event => {
+    inputEl?.addEventListener('keydown', event => {
       if (event.key === 'Enter' && movieStore.state.searchText.trim()) {
         // 검색창에 아무 것도 없을 때는 검색이 되지 않도록 함
         // 엔터키를 누르고, 검색창에 문자가 존재할 때만 검색이 되도록 만듦
@@ -27,7 +27,7 @@ export default class Search extends Component {
     })
 
     const btnEl = this.el.querySelector('.btn')
-    btnEl.addEventListener('click', () => {
+    btnEl?.addEventListener('click', () => {
       if (movieStore.state.searchText.trim()) {
         searchMovies(1)
       }
